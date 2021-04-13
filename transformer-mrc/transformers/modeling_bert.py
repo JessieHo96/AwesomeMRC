a4953b348f1e8f678feb8a19763604a7a4b30c95
JessieHo96 <hexiaofen96@gmial.com>
2021-04-13 21:32:18 +0800

    add gate_av

--- transformer-mrc/transformers/modeling_bert.py
+++ transformer-mrc/transformers/modeling_bert.py
@@ -2050,8 +2050,8 @@ class BertForQuestionAnsweringGateMechanism(BertPreTrainedModel):
         self.bias_1 = torch.nn.Parameter(torch.zeros(1,2, requires_grad = True))
         
         ##Branch_2
-        self.weight_2 = torch.nn.Parameter(torch.nn.init.normal_(torch.empty(self.config.hidden_size,2, requires_grad = True), std = 0.02))
-        self.bias_2 = torch.nn.Parameter(torch.zeros(1,2, requires_grad = True))
+        self.weight_2 = torch.Parameter(torch.nn.init.normal_(torch.empty(self.config.hidden_size,2, requires_grad = True), std = 0.02))
+        self.bias_2 = torch.Parameter(torch.zeros(1,2, requires_grad = True))
         
         
         self.relu = torch.nn.ReLU()
@@ -2077,7 +2077,7 @@ class BertForQuestionAnsweringGateMechanism(BertPreTrainedModel):
 
         sequence_output = outputs[0]
         
-        direct_logits = self.qa_outputs(sequence_output)##[batch_size, sequence_length, 2]
+        direct_logits = self.qa_output(sequence_output)##[batch_size, sequence_length, 2]
 
         final_hidden_size = sequence_output.size()
         
@@ -2134,6 +2134,159 @@ class BertForQuestionAnsweringGateMechanism(BertPreTrainedModel):
             start_loss = loss_fct(start_logits, start_positions)
             end_loss = loss_fct(end_logits, end_positions)
             total_loss = (start_loss + end_loss) / 2
+            outputs = (total_loss,) + outputs
+
+        return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
+
+    
+    
+class BertForQuestionAnsweringGateMechanismAVPool(BertPreTrainedModel):
+    r"""
+        **start_positions**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size,)``:
+            Labels for position (index) of the start of the labelled span for computing the token classification loss.
+            Positions are clamped to the length of the sequence (`sequence_length`).
+            Position outside of the sequence are not taken into account for computing the loss.
+        **end_positions**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size,)``:
+            Labels for position (index) of the end of the labelled span for computing the token classification loss.
+            Positions are clamped to the length of the sequence (`sequence_length`).
+            Position outside of the sequence are not taken into account for computing the loss.
+
+    Outputs: `Tuple` comprising various elements depending on the configuration (config) and inputs:
+        **loss**: (`optional`, returned when ``labels`` is provided) ``torch.FloatTensor`` of shape ``(1,)``:
+            Total span extraction loss is the sum of a Cross-Entropy for the start and end positions.
+        **start_scores**: ``torch.FloatTensor`` of shape ``(batch_size, sequence_length,)``
+            Span-start scores (before SoftMax).
+        **end_scores**: ``torch.FloatTensor`` of shape ``(batch_size, sequence_length,)``
+            Span-end scores (before SoftMax).
+        **hidden_states**: (`optional`, returned when ``config.output_hidden_states=True``)
+            list of ``torch.FloatTensor`` (one for the output of each layer + the output of the embeddings)
+            of shape ``(batch_size, sequence_length, hidden_size)``:
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        **attentions**: (`optional`, returned when ``config.output_attentions=True``)
+            list of ``torch.FloatTensor`` (one for each layer) of shape ``(batch_size, num_heads, sequence_length, sequence_length)``:
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention heads.
+
+    Examples::
+
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+        question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
+        input_text = "[CLS] " + question + " [SEP] " + text + " [SEP]"
+        input_ids = tokenizer.encode(input_text)
+        token_type_ids = [0 if i <= input_ids.index(102) else 1 for i in range(len(input_ids))]
+        start_scores, end_scores = model(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
+        all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
+        print(' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1]))
+        # a nice puppet
+
+
+    """
+    def __init__(self, config):
+        super(BertForQuestionAnsweringGateMechanismAVPool, self).__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config)
+        
+        ##Branch_1
+        self.weight_1 = torch.nn.Parameter(torch.nn.init.normal_(torch.empty(self.config.hidden_size,2, requires_grad = True), std = 0.02))
+        self.bias_1 = torch.nn.Parameter(torch.zeros(1,2, requires_grad = True))
+        
+        ##Branch_2
+        self.weight_2 = torch.Parameter(torch.nn.init.normal_(torch.empty(self.config.hidden_size,2, requires_grad = True), std = 0.02))
+        self.bias_2 = torch.Parameter(torch.zeros(1,2, requires_grad = True))
+        
+        
+        self.relu = torch.nn.ReLU()
+        
+        self.tanh = torch.nn.Tanh()
+        
+        self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels)##[batch_size, sequence_length, 2]
+        
+        
+        self.has_ans = nn.Sequential(nn.Dropout(p=config.hidden_dropout_prob), nn.Linear(config.hidden_size, 1))
+        
+        self.init_weights()
+        self.config = config
+
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
+                inputs_embeds=None, start_positions=None, end_positions=None, is_impossibles=None):
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+
+
+        sequence_output = outputs[0]
+        
+        first_word = sequence_output[:, 0, :]
+        
+        has_log = self.has_ans(first_word)
+        has_log = has_log.squeeze(-1)
+        
+        direct_logits = self.qa_output(sequence_output)##[batch_size, sequence_length, 2]
+
+        final_hidden_size = sequence_output.size()
+        
+        batch_size = final_hidden_size[0]
+        
+        sequence_length = final_hidden_size[1]##[batch_size, sequence_length, hidden_size]
+        
+        hidden_size = self.config.hidden_size
+        
+        final_hidden_reshape = sequence_output.view(-1,self.config.hidden_size)
+
+
+        #[batch_size*sequence_length,hidden_size] * [hidden_size,2] = [batch_size*sequence_length,2]
+        logits_1 = torch.matmul(final_hidden_reshape, self.weight_1)
+        logits_1 = self.bias_1 + logits_1 #[batch_size*sequence_length,2]
+        logits_1 = self.relu(logits_1)
+
+        ##Branch 2:
+        #[batch_size*sequence_length,hidden_size]
+        final_hidden_reshape = sequence_output.view(-1,self.config.hidden_size)
+
+
+        #[batch_size*sequence_length,hidden_size] * [hidden_size,2] = [batch_size*sequence_length,2]
+        logits_2 = torch.matmul(final_hidden_reshape,self.weight_2)
+        logits_2 = self.bias_2 + logits_2 #[batch_size*sequence_length,2]
+        logits_2 = self.tanh(logits_2)
+
+        #Gate Mechanism
+        logits = torch.mul(logits_1, logits_2)#[batch_size*sequence_length,2]
+        logits = logits.view(batch_size, -1, 2)## ##[batch_size, sequence_length, 2]
+        
+        
+        ##Residual
+        logits = logits + direct_logits####[batch_size, sequence_length, 2]
+
+
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        outputs = (start_logits, end_logits,) + outputs[2:]
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            
+            choice_loss = loss_fct(has_log, is_impossibles)
+            
+            total_loss = (start_loss + end_loss + choice_loss) / 3
             outputs = (total_loss,) + outputs
 
         return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
